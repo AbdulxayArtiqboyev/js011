3238dc5840496a57b9c40fb85c27cd73527bbd7d
AbdulxayArtiqboyev <ggithub164@gmail.com>
2024-06-26 01:07:48 +0500

    js011

---
+++ app.js
@@ -0,0 +1,129 @@
+// {
+
+//     // masala 1
+
+//     let array = [-5,3,5,-2];
+//     let result = [];
+//     array.forEach((son) =>{
+//         if(son > 0){
+//             result.push(son)
+//         } 
+//     }
+//     )
+//     console.log(result);
+    
+// }
+
+// {
+//     // masala 2
+
+//     // let str = (a) => a.split("").reverse().join("")
+//     // console.log(str('salom'));
+
+// }
+
+// {
+//     // masala 3 
+//     let number = (a) => a.split("").reverse().join("")
+//     console.log(number('321'));
+// }
+
+// {
+//     // masala 4
+//     let array = [false,true,false];
+//     let result = [];
+//     array.forEach((son) => {
+//         if(son !== true){
+//             result.push(son)
+//         }
+//     })
+//     console.log(result);
+// }
+
+
+// {
+    //     //  5 masalani
+    //     let foo = [1, 2, 3, 4, 5]
+    //     let sum = foo.filter((qiymat) => qiymat % 2 === 0)
+    //     console.log(sum);
+    // }
+    // 
+
+    // {
+    //     // 6 masala
+    //     function foo(array){
+    //         let sum = []
+    //      for(let i = 0, len = array.length; i < len; i++){
+    //       sum.push(array[i] ** 2)
+    //      }
+    //      return sum
+    //     }
+    //     console.log(foo([5, 2, 3]));
+    // }
+    // {
+    //     // 7 massala
+    //     function foo(n){
+    //         let sum = []
+    //         for(let i = 1; i <= n; i++){
+    //            sum.push(i)
+    //         }
+    //         return sum
+    //     }
+    //     console.log(foo(5));
+    
+    // }
+    // {
+    //     //  8 masala
+    //     function foo(a, b){
+    //         let sum = []
+    //         for(let i = a; i < b; i++){
+    //             if(i % 2 === 0){
+    //                 sum.push(i)
+    //             }
+    //         }
+    //         return sum
+    //     }
+    //     console.log(foo(2, 9));
+    // }
+    
+    // {
+    //     // 9 masala
+    //     function toCapitalize(text) {
+    //         text = text.split(".");
+    //         text2 = [];
+    //         text.forEach((txt) => {
+    //             txt = txt.trimStart();
+    //             txt2 = txt[0].toUpperCase();
+    //             txt = `${txt2}${txt
+    //                 .split("")
+    //                 .slice(1, txt.length - 0)
+    //                 .join("")}`;
+    //             text2.push(txt);
+    //         });
+    
+    //         return text2.join(". ");
+    //     }
+    //     console.log(toCapitalize("salom"));
+    
+    // {
+    //     // 10 masala
+    //     function foo(a, b) {
+    //       for (let i = a; i <= b; i++) {}
+    //     }
+    //     console.log(foo("salom", 3));
+    //   }
+    
+    // }
+    // {
+    //     // 11 masala 1 
+    //     let foo = [1,2,3]
+    //     foo.reverse()
+    //     console.log(foo);
+    // }
+    //
+    // {
+    //     // 12 masala
+    //     let foo = [2, 1, 13, 4]
+    //     foo.sort((a,b)=> a-b)
+    //     console.log(foo);
+    // }
